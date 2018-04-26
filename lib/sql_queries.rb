ae0663f43958627a1f1b763339e1ba7c0c59a165
David Ritchey <ritchey0713@gmail.com>
2018-04-26 02:36:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 JOIN pledges 
 ON projects.id = pledges.project_id 
 GROUP BY projects.title 
-HAVING amount_remaining >= 0 
+HAVING amount_remaining >= 0;
 "
 end
 
