c8a86a8869d21e5d4486191acba39fabe4a06d8d
David Ritchey <ritchey0713@gmail.com>
2018-04-26 02:36:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ HAVING amount_remaining >= 0
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount) AS total_pledges 
+"SELECT users.name, SUM(pledges.amount) AS total_pledges FROM users 
 JOIN pledges 
 ON users.id = pledges.user_id 
 GROUP BY users.name 
